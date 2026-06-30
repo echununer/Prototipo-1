9c88882a9d097251902cbdb7883d8ad0876a37fb
echununer <echununerpro@gmail.com>
2026-06-30 14:20:06 -0300

    le puse paneles a la ui osea al timer y al score

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,12 +19,12 @@ public class UIManager : MonoBehaviour
 
     public void UpdateScore(int score)
     {
-        txt_Score.text = "Score: " + score.ToString();
+        txt_Score.text = "Score:" + score.ToString() + "/5";
     }
 
     public void UpdateTime(float time)
     {
-        txt_Time.text = "Time: " + Mathf.CeilToInt(time).ToString();
+        txt_Time.text = "Time:" + Mathf.CeilToInt(time).ToString();
     }
 
     public void MostrarPantallaWin()
